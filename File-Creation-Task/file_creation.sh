5e0e49364971f8a14c66e47bd8c0d32d5d02511d
Ubuntu <ubuntu@ip-172-31-27-181.ec2.internal>
2026-08-15 11:46:31 +0000

    Add shell script for File Creation Task

---
+++ File-Creation-Task/file_creation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Part 1: Directory Setup & Concatenation
+mkdir -p my_folder
+cd my_folder
+echo "Hello from my_file" > my_file.txt
+echo "Hello from another_file" > another_file.txt
+cat another_file.txt >> my_file.txt
+cat my_file.txt
+ls -la
+
+# Part 2: Batch Creation, Rename, & Top 5 Latest
+touch file{01..20}.txt
+for f in file{01..05}.txt; do
+    mv "$f" "${f%.txt}.yml"
+done
+ls -lt | head -n 6

a3b4350f0181f974a47155f4c7a845e2c9314c50
Ubuntu <ubuntu@ip-172-31-13-17.eu-north-1.compute.internal>
2026-09-06 17:15:48 +0000

    Add app.sh inside Scripting-task/Jenkins-Task-2

---
+++ Jenkins-Task-2/app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "======================================="
+echo "  Jenkins Automated Build Execution"
+echo "  Triggered by GitHub Repository Event"
+echo "  Execution Date: $(date)"
+echo "======================================="

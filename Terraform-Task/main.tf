08674eb02d229e88ef981191eaca35ff63c49abc
Ubuntu <ubuntu@ip-172-31-39-236.eu-north-1.compute.internal>
2026-09-12 16:19:37 +0000

    Submit Terraform multi-region deployment task

---
+++ Terraform-Task/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Primary Provider: us-east-1
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+# Secondary Provider: us-west-2
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
+
+# Ensure default VPC exists in us-east-1
+resource "aws_default_vpc" "default_east" {
+  provider = aws.us_east_1
+}
+
+# Ensure default VPC exists in us-west-2
+resource "aws_default_vpc" "default_west" {
+  provider = aws.us_west_2
+}
+
+# Ubuntu 22.04 AMI Lookup for us-east-1
+data "aws_ami" "ubuntu_east" {
+  provider    = aws.us_east_1
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+# Ubuntu 22.04 AMI Lookup for us-west-2
+data "aws_ami" "ubuntu_west" {
+  provider    = aws.us_west_2
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+# EC2 Instance in us-east-1
+resource "aws_instance" "east_server" {
+  provider      = aws.us_east_1
+  ami           = data.aws_ami.ubuntu_east.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Instance-US-East-1"
+  }
+
+  depends_on = [aws_default_vpc.default_east]
+}
+
+# EC2 Instance in us-west-2
+resource "aws_instance" "west_server" {
+  provider      = aws.us_west_2
+  ami           = data.aws_ami.ubuntu_west.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Instance-US-West-2"
+  }
+
+  depends_on = [aws_default_vpc.default_west]
+}
+
+# Output Instance IDs
+output "us_east_1_instance_id" {
+  value       = aws_instance.east_server.id
+  description = "Instance ID in us-east-1"
+}
+
+output "us_west_2_instance_id" {
+  value       = aws_instance.west_server.id
+  description = "Instance ID in us-west-2"
+}
